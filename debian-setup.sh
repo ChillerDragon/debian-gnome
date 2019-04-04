c582ee0eef00899a0d06f2bd03d9e40a04f9347d
Chiller Dragon <chillerdragon@gmail.com>
2019-04-04 18:43:32 +0200

    Dont use sudo command before it is installed

--- debian-setup.sh
+++ debian-setup.sh
@@ -57,13 +57,15 @@ ask_for_razer
 #
 # You should now have an debian system running. It should work somehow already.
 # feel free to check if the network connection is working and upgrade the system:
-# The following steps are completly optional but strongly recommended.
-#
-# If you used the CD rom image the sources.list has to be updated
-# comment out the cdrom repos by hand or execute following command:
-sed '/^deb cdrom:.*/ s/deb cdrom:/# deb cdrom:/' /etc/apt/sources.list | sudo tee /etc/apt/sources.list
+# The following steps are completly optional but recommended.
+
 # Check if the network connection is working. And update the system.
 su root
+
+# If you used the CD rom image the sources.list has to be updated
+# comment out the cdrom repos by hand or execute following command:
+sed '/^deb cdrom:.*/ s/deb cdrom:/# deb cdrom:/' /etc/apt/sources.list | tee /etc/apt/sources.list
+
 apt-get update -y
 apt-get upgrade -y
 # Then install sudo so you don't have to use the root user
